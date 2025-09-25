37e0e192dd6bb6efd55e6dc0d922045d766affa7
Jose Jaime Come <josejaimecome@gmail.com>
2025-09-25 07:13:17 +0200

    python-range-function file added

---
+++ python-range-function.py
@@ -0,0 +1,15 @@
+
+# Loop through numbers from 0 up to, 
+# but not including, 5
+for i in range(5):
+    print(i)  # Print the current number
+
+# Loop through numbers from 2 up to, 
+# but not including, 6
+for i in range(2, 6):
+    print(i)  # Print the current number
+
+# Loop through numbers from 0 up to, but 
+# not including, 10, counting by 2
+for i in range(0, 10, 2):
+    print(i)  # Print the current number
